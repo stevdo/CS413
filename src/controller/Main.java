72f049caa535ee605adf9ea7ac9057130f7eecc1
Chris McCready <wjb12175@uni.strath.ac.uk>
2016-01-12 13:11:56 +0000

    Started device polling

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -28,28 +28,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		// open the system on the HomeScreen page
-		/*
-		 * NoticeBoardHomeScreen view = new NoticeBoardHomeScreen();
-		 * view.init_HomeScreen();
-		 */
-
-		// the following are tests, to open the system on other pages
-		/*
-		 * NoticeBoardNotesScreen notes_view = new NoticeBoardNotesScreen();
-		 * notes_view.init_SettingsScreen();
-		 */
-
-		/*
-		 * NoticeBoardSettingsScreen settings_view = new
-		 * NoticeBoardSettingsScreen(); settings_view.init_SettingsScreen();
-		 */
-
-		/*
-		 * NoticeBoardWarningsScreen warnings_view = new
-		 * NoticeBoardWarningsScreen(); warnings_view.init_WarningsScreen();
-		 */
-
 		boolean fileExists = false;
 
 		try {
@@ -100,10 +78,11 @@ public class Main {
 		mv.addKeyboardListener(keyboard_listener);
 		mv.addNotesListener(notes_listener);
 		m.setIndex();
-		mv.setIndex(m.getNotes().size()-1);
+		mv.setIndex(m.getNotes().size() - 1);
 		mv.setNotes(m.getNotes());
 		mv.init_NoticeboardFrame();
 		mv.updateWindow("home");
+		setTimer(m, mv);
 
 		// Cipher.simpleEncrypt("A B C D E F G");
 		// Cipher.simpleDecrypt("K L M N O P Q");
@@ -113,19 +92,16 @@ public class Main {
 		 * Cipher.messageDecrypt("Qod cywo wsvu Wkg");
 		 */
 
-		// after set amount of time?
-
-		// m.updateNotes();
-		// NoteList newNoteList = m.getNoteList();
-		// if (!(newNoteList.equals(MainView.getNoteList()))) {
-		// view.updateNoteList(newNoteList);
-		// }
-
 		// or do regardless of equality (does it matter? try both ways?)
 
-		// m.updateNotes();
-		// MainView.updateNoteList(m.getNoteList());
+	}
 
+	private static void setTimer(Model m, MainView mv) {
+		m.updateNotes();
+		m.setIndex();
+		mv.setIndex(m.getNotes().size() - 1);
+		mv.setNotes(m.getNotes());
+		setTimer(m, mv);
 	}
 
 	private static int getPin(String filePath) {
